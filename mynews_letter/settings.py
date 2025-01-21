7f924fc3daced5a5aa8c706c89a9cd89a9edb48e
Ndjekornom Victoire <126987926+ndjek1@users.noreply.github.com>
2025-01-21 21:50:44 +0300

    Update settings.py

--- mynews_letter/settings.py
+++ mynews_letter/settings.py
@@ -131,4 +131,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'vndjek@gmail.com' # Put your email here
-EMAIL_HOST_PASSWORD = 'jqjqbyjbuavnayos' # Put your password or app password
+EMAIL_HOST_PASSWORD = '' # Put your password or app password
